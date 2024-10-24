03f4dfbd96c6ddbbf7eb3a5bc5461c61b34ca1e2
Arif Pambudi <arifpambudi242@gmail.com>
2024-10-24 16:18:18 +0700

    ubah nama default db

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ dotenv.load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_for_development'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///cat_cpns.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite://db_nya.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
