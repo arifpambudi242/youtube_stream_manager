2c77ab2de343a786a282c3ae289b8d9dfdc00e1c
Arif Pambudi <arifpambudi242@gmail.com>
2024-10-29 10:11:59 +0700

    Refactor User model to add is_active field

--- app/models.py
+++ app/models.py
@@ -10,7 +10,8 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
-
+    is_active = db.Column(db.Boolean, default=False)
+    
     def set_password(self, password):
         """Hash password dan simpan ke password_hash"""
         self.password_hash = generate_password_hash(password)
